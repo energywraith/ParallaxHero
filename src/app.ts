dfe3d5ea9ad8757938990fe9d5aa9955bc65b29c
Adam Jałocha <energywraith@gmail.com>
2024-04-03 14:05:18 +0200

    Add ESLint and Prettier

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 function greet(name: string) {
-  const greeting = document.createElement("h1");
-  greeting.textContent = `Hello ${name}`;
-  document.body.appendChild(greeting);
+  const greeting = document.createElement('h1')
+  greeting.textContent = `Hello ${name}`
+  document.body.appendChild(greeting)
 }
 
-greet("Typescript");
+greet('Typescript')
